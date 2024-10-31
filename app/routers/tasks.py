2331b81c499aa00a8c70a6ad64af43f6bad2703f
Nasakin Artem <artnas379@gmail.com>
2024-10-31 16:11:42 +0300

    update: added some functionality

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,15 +1,36 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status, HTTPException
+
+from sqlalchemy.orm import Session
+from sqlalchemy import insert, select
+
+from app.backend.db_depends import get_db
+from app.schemas.schemas import CreateTask
+from app.models.tasks import Task
+from app.models.users import User
 
 
 router = APIRouter(prefix='/tasks', tags=['tasks'])
 
-@router.get('/')
-async def get_all_tasks():
-    pass
+@router.get('/', status_code=status.HTTP_200_OK)
+async def get_all_tasks(db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task).where(Task.status.is_(True))).all()
 
-@router.post('/')
-async def create_task():
-    pass
+    return tasks
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_task(db: Annotated[Session, Depends(get_db)], create_task: CreateTask):
+    db.execute(insert(Task).values(
+        title=create_task.title,
+        description=create_task.description,
+        status=create_task.status))
+    db.commit()
+
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Task created successfully'
+    }
 
 @router.put('/{task_id}')
 async def update_task(task_id: int):
