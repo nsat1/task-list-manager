ef49007229569fb4f65f2a814a6cbe77ea0c946c
Nasakin Artem <artnas379@gmail.com>
2024-11-01 00:37:05 +0300

    feat: add auth router

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from app.routers import tasks
+from app.routers import tasks, auth
 
 
 app = FastAPI()
 
 app.include_router(tasks.router)
+app.include_router(auth.router)
