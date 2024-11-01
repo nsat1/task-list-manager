0dce4235e3440766b0e36f29be4aa3d2cf9e6a17
Nasakin Artem <artnas379@gmail.com>
2024-11-01 07:43:13 +0300

    feat: add create_access_token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,7 @@
 from typing import Annotated
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt, JWTError, ExpiredSignatureError
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert, select
@@ -11,6 +14,7 @@ from passlib.context import CryptContext
 from app.models.users import User
 from app.schemas.schemas import CreateUser
 from app.backend.db_depends import get_db
+from app.backend.config import SECRET_KEY, ALGORITHM
 
 
 router = APIRouter(prefix='/auth', tags=['auth'])
@@ -43,8 +47,49 @@ async def authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], userna
 @router.post('/login')
 async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = await authenticate_user(db, form_data.username, form_data.password)
+    token = await create_access_token(user.username, user.id, expires_delta=timedelta(minutes=20))
 
     return {
-        'access_token': user.username,
+        'access_token': token,
         'token_type': 'bearer'
     }
+
+async def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        expire = payload.get('exp')
+
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate user'
+            )
+
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied"
+            )
+        return {
+            'username': username,
+            'user_id': user_id
+        }
+
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expired!"
+        )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='JWTError. Could not validate user'
+        )
