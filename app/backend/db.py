3bbe1e45a60ddd0ab0461a04db04a1b1d1df8801
Nasakin Artem <artnas379@gmail.com>
2024-11-01 09:42:10 +0300

    update: echo=false

--- app/backend/db.py
+++ app/backend/db.py
@@ -6,7 +6,7 @@ from .config import DB_NAME, DB_PASS, DB_USER, DB_HOST, DB_PORT
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 class Base(DeclarativeBase):
